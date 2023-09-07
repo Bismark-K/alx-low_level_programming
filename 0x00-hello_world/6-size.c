fd7917e93397711e3454afacc412b66aabe13c3e
Bismark Kwaku Kete <kbismarkkwaku@gmail.com>
2023-09-07 12:01:43 -0700

    Debugging task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,11 +9,6 @@
 
 int main(void)
 {
-int int1;
-char char1;
-long int long_int1;
-long long long l_l_i;
-float float1;
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
 printf("Size of an int: %lu byte(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
