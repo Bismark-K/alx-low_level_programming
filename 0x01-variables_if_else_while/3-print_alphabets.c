4ad012f58d8faf9241cc86a60c3113731eb5eba4
Bismark Kwaku Kete <kbismarkkwaku@gmail.com>
2023-09-08 15:34:12 +0000

    Debugging task 5

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success/correct)
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char i;
 	char j;
 
 	for (i = 'a'; i <= 'z'; i++)
+	{
 		putchar(i);
+	}	
 	for (j = 'A'; j <= 'Z'; j++)
+	{
 		putchar(j);
+	}
 	putchar('\n');
 	return (0);
 }
