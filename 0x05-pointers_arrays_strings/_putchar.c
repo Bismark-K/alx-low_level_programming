2ae82fe6c47fb6de8edec30903c30072fc3fbf90
dz1dzor <kbismarkkwaku@gmail.com>
2023-09-18 12:29:06 +0000

    Task 0. 98 battery st.

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _putchar - prints out the character c
+ *
+ * @c: the character to be printed out
+ *
+ * Author: dz1dzor
+ * Return: 1, if successful, -1 on error and errno is set appropriately.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
