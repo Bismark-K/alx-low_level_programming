98c2e8bd6a3aa9c703da8b12239a5e9ef9acde62
dz1dzor <kbismarkkwaku@gmail.com>
2023-09-18 12:20:38 +0000

    Task 0. 98 battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * reset-to_98 - updates the value after taking the pointer
+ *
+ * @n: expected int to be checked
+ *
+ * Author: dz1dzor
+ *
+ * Return: Always 0 (Success)
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is a directory for projects on pointers, arrays and strings
