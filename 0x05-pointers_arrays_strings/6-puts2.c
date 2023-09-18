585ed52e23b8ad31404d333425fa7c9c3aa63172
dz1dzor <kbismarkkwaku@gmail.com>
2023-09-18 14:37:02 +0000

    Task 6, Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts2 - prints out every other character of a string
+ *
+ * @str: where the characters to be printed are gotten from
+ *
+ * Author: dz1dzor
+ */
+
+void puts2(char *str)
+{
+	int i, lngth;
+
+	lngth = 0;
+
+	while (str[lngth] != '\0')
+	{
+		lngth++;
+	}
+
+	for (i = 0; i < lngth; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar(str[i];
+}
+
