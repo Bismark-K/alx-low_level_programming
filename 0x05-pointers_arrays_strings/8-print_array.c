dddb5d785ba1a67ae120d1ad1b5d29c33bcada96
dz1dzor <kbismarkkwaku@gmail.com>
2023-09-18 15:19:25 +0000

    Task 8 & 9, Arrays are not pointers  & strcpy

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_array - prints out n elements of an array of integers
+ *	followed by a new line!
+ *
+ * @a: expected array to print
+ * @n: number of elements
+ *
+ * Author: dz1dzor
+ */
+
+void print_array(int *a, int n)
+{
+	int t;
+
+	for (t = 0; t < n; t++)
+		if (t == 0)
+			printf("%d", a[t]);
+		else
+			printf(", %d", a[t]);
+
+	printf("\n");
+}
+
