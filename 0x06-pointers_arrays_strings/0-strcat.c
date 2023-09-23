83774f67caa8798116db3ddfa1e21a0c8c1f51c7
dz1dzor <kbismarkkwaku@gmail.com>
2023-09-23 01:35:43 +0000

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,7 +2,6 @@
 
 /**
  * _strcat - function for concatenating two strings
- *
  * @dest: resulting string
  * @src: source string
  *
@@ -18,9 +17,7 @@ char *_strcat(char *dest, char *src)
 	n = 0;
 	m = 0;
 	while (dest[n] != '\0')
-	{
 		n++;
-	}
 	while (src[m] != '\0')
 	{
 		dest[n] = src[m];
