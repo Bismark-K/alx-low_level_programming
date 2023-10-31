079a27bbb8205e21261ed93912c62176e2b1c47d
dz1dzor <kbismarkkwaku@gmail.com>
2023-10-31 22:44:14 +0000

    Debugged

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
  * Return: -1 on failure, 1 on success
  * Author: dz1dzor
 */
-int create_file(const *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int fl, wtstat, wrd;
 
