e3afc7076c964c5a659b3cd225b3e73d55d3a05f
dz1dzor <kbismarkkwaku@gmail.com>
2024-04-11 11:58:20 +0000

    Binary search

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,71 @@
+#include "search_algos.h"
+
+
+int recursive_srch(int *array, size_t size, int value);
+
+/**
+ * binary_search - searches for a value in a sorted array of integers
+ * @array: pointer to the first element of the array to search in
+ * @size: total elements in the array
+ * @value: the element to search for
+ * Return: the index of the found value, -1 if array is NULL or
+ *	value isn't found
+ * Author: Bismark-K
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int i;
+
+	/** get the index **/
+	i = recursive_srch(array, size, value);
+
+	/* if the value isn't found */
+	if (array[i] != value)
+		return (-1);
+
+	if (i != -1)
+		printf("Value %d found at index %d\n", value, i);
+	else
+		printf("Value %d not found in the array\n", value);
+
+	return (i);
+}
+
+/**
+ * recursive_srch - function to do the searching
+ * @array: pointer to the first element of the array to search in
+ * @size: the size of the area
+ * @value: the value to search for
+ * Return: the index
+ */
+int recursive_srch(int *array, size_t size, int value)
+{
+	size_t i, rem = size / 2;
+
+	if (size == 0 || array == NULL)
+		return (-1);
+
+	/* Print the array been searched now */
+	printf("Searching in array: ");
+	for (i = 0; i < size; i++)
+	{
+		printf("%d", array[i]);
+		if (i < size - 1)
+			printf(", ");
+	}
+	printf("\n");
+
+	/* start search */
+	if (size % 2 == 0 && rem)
+		rem--;
+
+	if (array[rem] == value)
+		return ((int)rem);
+
+	if (array[rem] > value)
+		return (recursive_srch(array, rem, value));
+
+	rem++;
+
+	return (recursive_srch(array + rem, size - rem, value) + rem);
+}
